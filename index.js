bc9f2ee96a6e08c64bf2be17701bf39c30f1d0e8
Tiger Chen <tigerchen@Tigers-MBP-2.modem>
2020-02-28 21:46:01 +1100

    added proxyKey to myVue

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-function myVue (data, el, expression) {
-  this.data = data;
-  observe(data);
-  el.innerHTML = this.data[expression];
-  new Watcher(this, expression, function(value) {
-    el.innerHTML = value;
-  })
+class myVue{
+  constructor(data, el, expressioin) {
+    this.data = data;
+    this.el = el;
+    this.expressioin = expressioin;
 
-  return this;
+    this.run();
+  }
+
+  run() {
+    observe(this.data);
+    this.el.innerHTML = this.data[this.expressioin];
+    new Watcher(this, this.expressioin, value => {
+      this.el.innerHTML = value;
+    });
+    
+    Object.keys(this.data).map(key => this.proxyKey(key));
+  }
+
+  proxyKey(key) {
+    Object.defineProperty(this, key, {
+      enumerable: true,
+      configurable: true,
+      get: () => {
+        return this.data[key];
+      },
+      set: (newValue) => {
+        this.data[key] = newValue;
+      }
+    });
+  }
 }
